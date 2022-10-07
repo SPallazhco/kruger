323068693380b998afa199df1cab2d2309950988
SPallazhco <spallazhco@baustro.fin.ec>
2022-10-07 16:36:44 -0500

    OAUTH

--- kruger/src/main/java/ec/kruger/KrugerApplication.java
+++ kruger/src/main/java/ec/kruger/KrugerApplication.java
@@ -2,8 +2,12 @@ package ec.kruger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableResourceServer
+@Configuration
 public class KrugerApplication {
 
     public static void main(String[] args) {
---
+++ kruger/src/main/java/ec/kruger/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,47 @@
+package ec.kruger.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.ws.config.annotation.EnableWs;
+
+/**
+ * Clase donde se encuentran los beans de configuracion.
+ */
+
+@EnableWebSecurity
+@EnableWs
+@Configuration
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/swagger-ui/**", "/v3/api-docs/**");
+    }
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.requestMatchers().antMatchers("/administrador")
+                .and().authorizeRequests()
+                .antMatchers("/administrador").access("hasRole('ADMIN')");
+                //.and().requestMatchers().antMatchers("/user")
+                //.and().authorizeRequests()
+                //.antMatchers("/user").access("hasRole('USER')");
+    }
+}
